a1f0e063e24ea9b0f592f8ee51d9f64ab39fb63d
breno085 <br3noacioli@gmail.com>
2024-07-19 20:36:44 -0300

    migrations

--- ModuloAPI/obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
+++ ModuloAPI/obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2149ec2811ec7aefcbc119c2c8b83b1e53778e20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee57b3a352c20e4cf2fd2e3b32df1d601cc4da4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
