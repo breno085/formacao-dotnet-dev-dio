095369c127ddce2420b76e49c7f39e6ec53a151a
breno085 <br3noacioli@gmail.com>
2024-07-19 23:23:07 -0300

    Transfer uncommitted changes from backup

--- ModuloAPI/obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
+++ ModuloAPI/obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1f0e063e24ea9b0f592f8ee51d9f64ab39fb63d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9df2c24febd9b10f1d2debd221c393bd90325fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
